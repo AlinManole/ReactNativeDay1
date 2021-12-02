011fb1431275f04078b7b3cac54702a72f758963
Alin Manole <alin.manole@yahoo.com>
2021-12-02 13:45:43 +0100

    update final

--- App.js
+++ App.js
@@ -1,29 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, Button, Alert, ActivityIndicator } from 'react-native';
+import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Image,
+  Pressable,
+  Alert,
+  ActivityIndicator
+} from "react-native"
 
-export default function App() {
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "blue",
+    padding: 10,
+    marginTop: 50,
+  },
+  textContainer: {
+    backgroundColor: "white",
+    paddingBottom: 50
+  },
+  margin80: {
+    marginVertical: 80
+  },
+  text1: {
+    fontSize: 30
+  },
+  text2: {
+    textAlign: "center"
+  },
+  text3: {
+    fontWeight: "bold"
+  },
+  image: {
+    width: 124,
+    height: 40,
+    alignSelf: "center",
+    marginVertical: 40
+  },
+  button: {
+    backgroundColor: "red",
+    padding: 30,
+    width: 150,
+    alignSelf: "center",
+    marginVertical: 90
+  }
+})
 
-
-  
+const App = () => {
+  const createAlert = () => {
+    Alert.alert(
+      "Mon titre",
+      "Mon message",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed")
+        },
+        {
+          text: "OK",
+          onPress: () => console.log("OK Pressed")
+        }
+      ]
+    )
+  }
 
   return (
     <ScrollView style={styles.container}>
-      <View style={styles.innerContainer}>
-        <Text style={styles.text1}>Text 1</Text>
-        <Text style={styles.text2}>Text 2</Text>
-        <Text style={styles.text3}>Text 3</Text>
-        <ActivityIndicator size="large" color="#00000" />
-      <Image  style={{width: 100, margin: 15, height: 30}} source={{
-        uri: 'https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png',
-      }} />
-      <Image source={require('./assets/konexio-logo_1.png')}
-      />
-      <Button onPress={() => Alert.alert("Don`t Touch me!")} title="Click ME!"></Button>
+      <View style={styles.textContainer}>
+        <Text style={[styles.margin80, styles.text1]}>Hello app</Text>
+        <Text style={[styles.margin80, styles.text2]}>Hello app</Text>
+        <Text style={[styles.margin80, styles.text3]}>Hello app</Text>
+
+        <Image
+          style={styles.image}
+          source={{
+            uri: "https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png"
+          }}
+        />
+        <Image
+          style={styles.image}
+          source={require("./assets/konexio.png")}
+        />
+
+        <Pressable style={styles.button} onPress={createAlert}>
+          <Text>Press me!</Text>
+        </Pressable>
+
+        <ActivityIndicator
+          color="blue"
+          size="large"
+        />
       </View>
     </ScrollView>
   )
 }
+
+export default App
 
 
 
